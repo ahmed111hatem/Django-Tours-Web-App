567e552ccd5435f0d058df1c9c49e605017670f1
ahmed111hatem <ahmed111hatem111@gmail.com>
2025-07-08 17:27:47 +0300

    New Update

--- worldtour/settings.py
+++ worldtour/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k7%wj7v%!htp9w!ykebj&2f%*(1n@+)g3m545^54lqdxu7yeu$"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
